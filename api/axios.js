fdb4125c8ed75fa0df58ff283845809fc37fba89
jay-2506 <jpatel9003@gmail.com>
2026-02-27 16:14:46 +0530

    eddite

--- api/axios.js
+++ api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://spotify-clone-6qrx.onrender.com",
+  baseURL: "https://spotify-clone-6qrx.onrender.com/api",
   withCredentials: true, // for cookie JWT
 });
 
